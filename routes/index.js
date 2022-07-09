d5cef00ea0f6f653f543cb020fdf0b18e4803d78
Caio <caiosobreira77@gmail.com>
2022-07-09 13:39:47 -0300

    products

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-
+import postRegister from "../controllers/authController"
 //import controller from "./controllerFuturo"
 
 const router = Router();
 
 //router.use(controllerUsado);
+router.use("/register", postRegister)
 
 export default router;
