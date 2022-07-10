b332bd89eeebaa46d32f0bc045f7716cc13e134f
leliovictor <leliov.vieira@gmail.com>
2022-07-10 18:38:27 -0300

    Fix bug - forgot to import cart router at index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import authRouter from "./authRouter.js";
+import cartRouter from "./cartRouter.js";
 import postRegister from "../controllers/authController.js";
 
 const router = Router();
 
 router.use("/register", postRegister);
 router.use(authRouter);
+router.use(cartRouter);
 
 export default router;
