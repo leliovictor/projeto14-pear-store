582871ee46895e49c018cff26ef914b1ed9d1759
Caio <caiosobreira77@gmail.com>
2022-07-11 16:09:11 -0300

    add products router

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import registerRouter from "./registerRouter"
+import registerRouter from "./registerRouter.js"
 import authRouter from "./authRouter.js";
 import cartRouter from "./cartRouter.js";
-
+import productsRouter from "./productsRouter.js"
 
 const router = Router();
 
+router.use(productsRouter);
 router.use(registerRouter);
 router.use(authRouter);
 router.use(cartRouter);
