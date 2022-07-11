8ebc6b644628f390450c0b521330082dff51ab37
leliovictor <leliov.vieira@gmail.com>
2022-07-11 00:56:49 -0300

    Added delete cart item in API

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,42 @@
+import db from "../config/db";
+
 export function getCart(_req, res) {
   const { cart } = res.locals;
 
   return res.status(202).send(cart);
+}
+
+export function deleteCartItem(req, res) {
+  const { email } = req.locals.data;
+  const { cart } = req.locals;
+
+  const index = req.params;
+
+  cart.splice(index,1);
+
+  try {
+    await db.collection("users").updateOne({email}, {$set: {cart}});
+
+    res.sendStatus(202);
+  } catch(err) {
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteStatement(req, res) {
+  const { walletStatement, user_id } = res.locals.walletStatement;
+
+  const { index } = req.params;
+
+  walletStatement.splice(index, 1);
+
+  try {
+    await db
+      .collection("statements")
+      .updateOne({ user_id: user_id }, { $set: { walletStatement } });
+
+    res.sendStatus(202);
+  } catch (err) {
+    res.sendStatus(500);
+  }
 }
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { getCart } from "../controllers/cartController.js";
+import { getCart, deleteCartItem } from "../controllers/cartController.js";
 import { checkToken, findUserCart } from "../middlewares/cartMiddlewares.js";
 
 const cartRouter = Router();
 
 cartRouter.get("/cart", checkToken, findUserCart, getCart);
+cartRouter.delete("/cart/:index", checkToken, findUserCart, deleteCartItem);
 
 export default cartRouter;
